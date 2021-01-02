4838cd93d8f0f53ccbecc14a8b50b3c57b197cbb
AlesLong <yevbes@gmail.com>
2021-01-02 18:09:07 +0200

    [Task2006]

---
+++ 2.JavaCore/src/com/javarush/task/task20/task2003/Solution.java
@@ -0,0 +1,36 @@
+package com.javarush.task.task20.task2003;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+/* 
+Знакомство с properties
+*/
+
+public class Solution {
+
+    public static Map<String, String> runtimeStorage = new HashMap<>();
+
+    public static void save(OutputStream outputStream) throws Exception {
+        PrintWriter writer = new PrintWriter(outputStream);
+
+
+    }
+
+    public static void load(InputStream inputStream) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+    }
+
+    public static void main(String[] args) {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+             FileInputStream fos = new FileInputStream(reader.readLine())) {
+            load(fos);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(runtimeStorage);
+    }
+}
