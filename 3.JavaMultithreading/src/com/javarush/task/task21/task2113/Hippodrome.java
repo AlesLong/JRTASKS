df28f4718b1aa32b406857aeeae06c976c2666a5
AlesLong <yevbes@gmail.com>
2021-01-05 17:07:00 +0200

    [Task2113]

--- 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
@@ -4,6 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Hippodrome {
+    public Hippodrome(List<Horse> horses) {
+        this.horses = horses;
+    }
+
     public List<Horse> getHorses() {
         return horses;
     }
