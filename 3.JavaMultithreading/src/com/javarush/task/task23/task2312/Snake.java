17e91bb0fe9dd85cbf769fb8baaf5041c16af9cb
AlesLong <yevbes@gmail.com>
2021-01-06 02:42:47 +0200

    [Task2312]

--- 3.JavaMultithreading/src/com/javarush/task/task23/task2312/Snake.java
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2312/Snake.java
@@ -41,6 +41,23 @@ public class Snake {
     }
 
     public void move() {
+        if (isAlive) {
+            if (direction == SnakeDirection.UP) {
+                move(0, -1);
+            }
+            if (direction == SnakeDirection.RIGHT) {
+                move(1, 0);
+            }
+            if (direction == SnakeDirection.DOWN) {
+                move(0, 1);
+            }
+            if (direction == SnakeDirection.LEFT) {
+                move(-1, 0);
+            }
+        }
+    }
+
+    public void move(int a, int b) {
 
     }
 }
