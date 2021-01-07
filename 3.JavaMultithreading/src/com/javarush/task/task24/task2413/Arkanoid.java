2cd7964dfcab6b61060b81ff66c9213e098a32fa
AlesLong <yevbes@gmail.com>
2021-01-07 14:29:35 +0200

    [Task2413]

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Arkanoid.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Arkanoid.java
@@ -44,7 +44,16 @@ public class Arkanoid {
     }
 
     void move() {
-        
+        stand.move();
+        ball.move();
+    }
+
+    void draw(Canvas canvas) {
+        ball.draw(canvas);
+        stand.draw(canvas);
+        for (Brick brick : bricks) {
+            brick.draw(canvas);
+        }
     }
 
 
