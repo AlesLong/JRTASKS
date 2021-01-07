d3850081ce8cbc10e5a036460a5b0e809debfeda
AlesLong <yevbes@gmail.com>
2021-01-07 14:18:51 +0200

    [Task2413]

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Ball.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Ball.java
@@ -37,11 +37,18 @@ public class Ball extends BaseObject {
 
     @Override
     void draw(Canvas canvas) {
-
+        canvas.setPoint(x, y, 'O');
     }
 
     @Override
     void move() {
+        if (!isFrozen) {
+            x += dx;
+            y += dy;
+        }
+    }
 
+    void start() {
+        isFrozen = false;
     }
 }
