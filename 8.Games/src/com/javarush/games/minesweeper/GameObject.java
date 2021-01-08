9663b76d76a8c76338f77b7fffe7282b5d2595e2
AlesLong <yevbes@gmail.com>
2021-01-08 15:53:51 +0200

    [MineSweeperGame]

--- 8.Games/src/com/javarush/games/minesweeper/GameObject.java
+++ 8.Games/src/com/javarush/games/minesweeper/GameObject.java
@@ -1,7 +1,5 @@
 package com.javarush.games.minesweeper;
 
-import com.javarush.engine.cell.Game;
-
 public class GameObject {
     public int x;
     public int y;
@@ -14,6 +12,6 @@ public class GameObject {
     public GameObject(int x, int y, boolean isMine) {
         this.x = x;
         this.y = y;
-        this.isMine = isMine;
+       this.isMine = isMine;
     }
 }
