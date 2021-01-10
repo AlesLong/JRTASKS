e22facea6783b1fe5b7309c99ef86d12495aece8
AlesLong <yevbes@gmail.com>
2021-01-10 16:08:28 +0200

    [RacerGame]

--- 8.Games/src/com/javarush/games/racer/PlayerCar.java
+++ 8.Games/src/com/javarush/games/racer/PlayerCar.java
@@ -1,11 +1,30 @@
 package com.javarush.games.racer;
 
 public class PlayerCar extends GameObject {
+
+    private Direction direction;
     private static int playerCarHeight = ShapeMatrix.PLAYER.length;
     public int speed = 1;
 
     public PlayerCar() {
         super(RacerGame.WIDTH / 2 + 2, RacerGame.HEIGHT - playerCarHeight - 1, ShapeMatrix.PLAYER);
+    }
+
+    public void setDirection(Direction direction) {
+        this.direction = direction;
+    }
+
+    public Direction getDirection() {
+        return direction;
+    }
+
+    public void move() {
+        if (direction == Direction.LEFT) {
+            x--;
+        }
+        if (direction == Direction.RIGHT) {
+            x++;
+        }
     }
 
 }
