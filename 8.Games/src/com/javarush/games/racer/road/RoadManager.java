77e3478a1a1a6876b6e051c054e1cd18e101f948
AlesLong <yevbes@gmail.com>
2021-01-10 18:41:43 +0200

    [RacerGame]

--- 8.Games/src/com/javarush/games/racer/road/RoadManager.java
+++ 8.Games/src/com/javarush/games/racer/road/RoadManager.java
@@ -19,7 +19,7 @@ public class RoadManager {
     private RoadObject createRoadObject(RoadObjectType type, int x, int y) {
         if (type.equals(RoadObjectType.THORN)) {
             return new Thorn(x, y);
-        } else return null;
+        } else return new Car(type, x, y);
     }
 
     private void addRoadObject(RoadObjectType type, Game game) {
@@ -61,6 +61,7 @@ public class RoadManager {
 
     public void generateNewRoadObjects(Game game) {
         generateThorn(game);
+        generateRegularCar(game);
     }
 
     private void deletePassedItems() {
@@ -74,6 +75,13 @@ public class RoadManager {
             }
         }
         return false;
+    }
+
+    private void generateRegularCar(Game game) {
+        int carTypeNumber = game.getRandomNumber(4);
+        if (game.getRandomNumber(100) < 30) {
+            addRoadObject(RoadObjectType.values()[carTypeNumber], game);
+        }
     }
 
 }
