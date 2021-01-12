3a16846724a8fcfdce219533ebc74d4d11a54e14
AlesLong <yevbes@gmail.com>
2021-01-12 14:19:38 +0200

    [SpaceInvadersGame]

--- 8.Games/src/com/javarush/games/spaceinvaders/SpaceInvadersGame.java
+++ 8.Games/src/com/javarush/games/spaceinvaders/SpaceInvadersGame.java
@@ -2,10 +2,15 @@ package com.javarush.games.spaceinvaders;
 
 import com.javarush.engine.cell.Game;
 import com.javarush.engine.cell.*;
+import com.javarush.games.spaceinvaders.gameobjects.Star;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SpaceInvadersGame extends Game {
     public static final int WIDTH = 64;
     public static final int HEIGHT = 64;
+    private List<Star> stars;
 
     @Override
     public void initialize() {
@@ -14,6 +19,7 @@ public class SpaceInvadersGame extends Game {
     }
 
     private void createGame() {
+        createStars();
         drawScene();
     }
 
@@ -26,6 +32,16 @@ public class SpaceInvadersGame extends Game {
             for (int x = 0; x < WIDTH; x++) {
                 setCellValueEx(x, y, Color.BLACK, "");
             }
+        }
+        for (Star star : stars) {
+            star.draw(this);
+        }
+    }
+
+    private void createStars() {
+        stars = new ArrayList<>();
+        for (int i = 0; i < 8; i++) {
+            stars.add(new Star(Math.random() * WIDTH, Math.random() * HEIGHT));
         }
     }
 }
