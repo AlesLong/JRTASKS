e45d0811e6c4946fe779c00e7fcab19c0594febc
AlesLong <yevbes@gmail.com>
2021-01-13 14:34:54 +0200

    [SpaceInvadersGame]

--- 8.Games/src/com/javarush/games/spaceinvaders/gameobjects/EnemyFleet.java
+++ 8.Games/src/com/javarush/games/spaceinvaders/gameobjects/EnemyFleet.java
@@ -2,6 +2,7 @@ package com.javarush.games.spaceinvaders.gameobjects;
 
 import com.javarush.engine.cell.Game;
 import com.javarush.games.spaceinvaders.ShapeMatrix;
+import com.javarush.games.spaceinvaders.SpaceInvadersGame;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -28,6 +29,26 @@ public class EnemyFleet {
 
     public void draw(Game game) {
         ships.forEach(enemyShip -> enemyShip.draw(game));
+    }
+
+    private double getLeftBorder() {
+        double left = SpaceInvadersGame.WIDTH;
+        for (GameObject ship : ships) {
+            if (ship.x < left) {
+                left = ship.x;
+            }
+        }
+        return left;
+    }
+
+    private double getRightBorder() {
+        double right = 0;
+        for (GameObject ship : ships) {
+            if (ship.x + ship.width > right) {
+                right = ship.x + ship.width;
+            }
+        }
+        return right;
     }
 }
 
