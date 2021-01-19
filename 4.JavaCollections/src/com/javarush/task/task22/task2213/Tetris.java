418da673a57968d9ff78fd88bdcec1ea6ff8b3dd
AlesLong <yevbes@gmail.com>
2021-01-19 19:05:47 +0200

    [Task2213Tetris]

--- 4.JavaCollections/src/com/javarush/task/task22/task2213/Tetris.java
+++ 4.JavaCollections/src/com/javarush/task/task22/task2213/Tetris.java
@@ -2,6 +2,23 @@ package com.javarush.task.task22.task2213;
 
 public class Tetris {
     private Field field;
+
+    public Field getField() {
+        return field;
+    }
+
+    public void setField(Field field) {
+        this.field = field;
+    }
+
+    public Figure getFigure() {
+        return figure;
+    }
+
+    public void setFigure(Figure figure) {
+        this.figure = figure;
+    }
+
     private Figure figure;
 
 
