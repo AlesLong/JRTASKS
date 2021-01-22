a8dbae1e301ccff7cbbfb62b450107fa24c20508
AlesLong <yevbes@gmail.com>
2021-01-22 16:04:27 +0200

    [Task2515Space]

--- 4.JavaCollections/src/com/javarush/task/task25/task2515/Space.java
+++ 4.JavaCollections/src/com/javarush/task/task25/task2515/Space.java
@@ -22,6 +22,18 @@ public class Space {
         bombs = new ArrayList<>();
     }
 
+    public void run() {
+
+    }
+
+    public void draw() {
+
+    }
+
+    public void sleep(int ms) {
+
+    }
+
     public void setShip(SpaceShip ship) {
         this.ship = ship;
     }
