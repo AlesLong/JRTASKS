97691d1d341678a681572e1ca8f4f4f1c97dbe35
AlesLong <yevbes@gmail.com>
2021-01-23 17:47:01 +0200

    [Task3707CustomSet]

--- 4.JavaCollections/src/com/javarush/task/task37/task3707/AmigoSet.java
+++ 4.JavaCollections/src/com/javarush/task/task37/task3707/AmigoSet.java
@@ -1,5 +1,8 @@
 package com.javarush.task.task37.task3707;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.util.*;
 
@@ -51,6 +54,28 @@ public class AmigoSet<E> extends AbstractSet<E> implements Cloneable, Serializab
         if (map.containsKey(e)) {
             return true;
         } else return false;
+    }
+
+    private void writeObject(ObjectOutputStream s) throws java.io.IOException {
+        s.defaultWriteObject();
+        s.writeInt(HashMapReflectionHelper.<Integer>callHiddenMethod(map, "capacity"));
+        s.writeFloat(HashMapReflectionHelper.<Float>callHiddenMethod(map, "loadFactor"));
+        s.writeInt(map.size());
+        for (E e : map.keySet())
+            s.writeObject(e);
+    }
+
+
+    private void readObject(ObjectInputStream s) throws java.io.IOException, ClassNotFoundException {
+        s.defaultReadObject();
+        int capacity = s.readInt();
+        float loadFactor = s.readFloat();
+        map = new HashMap<>(capacity, loadFactor);
+        int size = s.readInt();
+        for (int i = 0; i < size; i++) {
+            E e = (E) s.readObject();
+            map.put(e, PRESENT);
+        }
     }
 
     @Override
