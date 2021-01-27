aed1eab86d2472d4e5128afaffc0dc809c94d99a
AlesLong <yevbes@gmail.com>
2021-01-27 21:02:59 +0200

    [Task2909Refactoring]

--- 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/Teacher.java
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/Teacher.java
@@ -1,12 +1,12 @@
 package com.javarush.task.task29.task2909.human;
 
-public class Teacher extends Human {
+public class Teacher extends UniversityPerson {
     protected int course;
     private int numberOfStudents;
-    private String university;
+
 
     public Teacher(String name, int age, int numberOfStudents) {
-        super(name,age);
+        super(name, age);
         this.name = name;
         this.age = age;
         this.numberOfStudents = numberOfStudents;
@@ -19,13 +19,7 @@ public class Teacher extends Human {
     public void teach() {
     }
 
-    public String getUniversity() {
-        return university;
-    }
 
-    public void setUniversity(String university) {
-        this.university = university;
-    }
 
     public void printData() {
         System.out.println("Преподаватель: " + name);
