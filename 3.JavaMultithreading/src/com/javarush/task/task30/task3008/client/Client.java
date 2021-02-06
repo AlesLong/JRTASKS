5c164ab492582df17bb737f883db33ddbdf5a2a4
AlesLong <yevbes@gmail.com>
2021-02-06 20:02:04 +0200

    [Task3008]

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/Client.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/Client.java
@@ -44,9 +44,38 @@ public class Client {
             synchronized (Client.this) {
                 Client.this.notify();
             }
-
         }
 
+        protected void clientHandshake() throws IOException, ClassNotFoundException {
+            while (true) {
+                Message message = connection.receive();
+                if (message.getType() == MessageType.NAME_REQUEST) {
+                    String name = getUserName();
+                    connection.send(new Message(MessageType.USER_NAME, name));
+
+                } else if (message.getType() == MessageType.NAME_ACCEPTED) {
+                    notifyConnectionStatusChanged(true);
+                    return;
+                } else {
+                    throw new IOException("Unexpected MessageType");
+                }
+            }
+        }
+
+        protected void clientMainLoop() throws IOException, ClassNotFoundException {
+            while (true) {
+                Message message = connection.receive();
+                if (message.getType() == MessageType.TEXT) { // Сервер прислал сообщение с текстом
+                    processIncomingMessage(message.getData());
+                } else if (MessageType.USER_ADDED == message.getType()) {
+                    informAboutAddingNewUser(message.getData());
+                } else if (MessageType.USER_REMOVED == message.getType()) {
+                    informAboutDeletingNewUser(message.getData());
+                } else {
+                    throw new IOException("Unexpected MessageType");
+                }
+            }
+        }
     }
 
     protected SocketThread getSocketThread() {
