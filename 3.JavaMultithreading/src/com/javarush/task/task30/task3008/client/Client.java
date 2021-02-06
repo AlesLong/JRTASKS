0733f440c0094b38e5c3c2f378ced30bfbf59263
AlesLong <yevbes@gmail.com>
2021-02-06 19:35:26 +0200

    [Task3008]

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/Client.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/Client.java
@@ -27,6 +27,26 @@ public class Client {
     }
 
     public class SocketThread extends Thread {
+        protected void processIncomingMessage(String message) {
+            ConsoleHelper.writeMessage(message);
+        }
+
+        protected void informAboutAddingNewUser(String userName) {
+            ConsoleHelper.writeMessage(MessageType.USER_ADDED + ": " + userName);
+        }
+
+        protected void informAboutDeletingNewUser(String userName) {
+            ConsoleHelper.writeMessage(MessageType.USER_REMOVED + ": " + userName);
+        }
+
+        protected void notifyConnectionStatusChanged(boolean clientConnected) {
+            Client.this.clientConnected = clientConnected;
+            synchronized (Client.this) {
+                Client.this.notify();
+            }
+
+        }
+
     }
 
     protected SocketThread getSocketThread() {
