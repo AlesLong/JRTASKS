5906deee1cdb619e9f4e32067cdb888bc0527104
AlesLong <yevbes@gmail.com>
2021-02-06 18:12:04 +0200

    [Task3008]

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/Client.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/Client.java
@@ -1,19 +1,51 @@
 package com.javarush.task.task30.task3008.client;
 
 import com.javarush.task.task30.task3008.Connection;
+import com.javarush.task.task30.task3008.ConsoleHelper;
+import com.javarush.task.task30.task3008.Message;
+import com.javarush.task.task30.task3008.MessageType;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Client {
 
     protected Connection connection;
     private volatile boolean clientConnected = false;
 
+    protected String getServerAddress() {
+        ConsoleHelper.writeMessage("Введите адрес сервера:");
+        return ConsoleHelper.readString();
+    }
+
+    protected int getServerPort() {
+        ConsoleHelper.writeMessage("Введите порт сервера:");
+        return ConsoleHelper.readInt();
+    }
+
+    protected String getUserName() {
+        ConsoleHelper.writeMessage("Введите ваше имя:");
+        return ConsoleHelper.readString();
+    }
 
     public class SocketThread extends Thread {
+    }
 
+    protected SocketThread getSocketThread() {
+        return new SocketThread();
+    }
 
-        @Override
-        public void run() {
-            super.run();
+    protected void sendTextMessage(String text) {
+        try {
+            connection.send(new Message(MessageType.TEXT, text));
+        } catch (IOException e) {
+            ConsoleHelper.writeMessage("Не удалось отправить сообщение");
+            clientConnected = false;
         }
+    }
+
+    protected boolean shouldSendTextFromConsole() {
+        return true;
     }
 }
