f28008ab66884d326b4ed6da7c3527dab8aa59ad
AlesLong <yevbes@gmail.com>
2021-02-07 14:50:42 +0200

    [Task3008]

---
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/ClientGuiController.java
@@ -0,0 +1,82 @@
+package com.javarush.task.task30.task3008.client;
+
+
+public class ClientGuiController extends Client {
+    public ClientGuiModel getModel() {
+        return model;
+    }
+
+    public void setModel(ClientGuiModel model) {
+        this.model = model;
+    }
+
+    private ClientGuiModel model = new ClientGuiModel();
+    private ClientGuiView view = new ClientGuiView(this);
+
+    @Override
+    protected SocketThread getSocketThread() {
+        return new GuiSocketThread();
+    }
+
+    @Override
+    public void run() {
+        getSocketThread().run();
+    }
+
+    @Override
+    protected String getServerAddress() {
+        return view.getServerAddress();
+    }
+
+    @Override
+    protected int getServerPort() {
+        return view.getServerPort();
+    }
+
+    @Override
+    protected String getUserName() {
+        return view.getUserName();
+    }
+
+
+    @Override
+    protected void sendTextMessage(String text) {
+        super.sendTextMessage(text);
+    }
+
+    public class GuiSocketThread extends SocketThread {
+        @Override
+        protected void processIncomingMessage(String message) {
+            model.setNewMessage(message);
+            view.refreshMessages();
+        }
+
+        @Override
+        protected void informAboutAddingNewUser(String userName) {
+            model.addUser(userName);
+
+            view.refreshUsers();
+        }
+
+
+        @Override
+        protected void informAboutDeletingNewUser(String userName) {
+            model.deleteUser(userName);
+            view.refreshUsers();
+        }
+
+
+        @Override
+        protected void notifyConnectionStatusChanged(boolean clientConnected) {
+            view.notifyConnectionStatusChanged(clientConnected);
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+        ClientGuiController clientGuiController = new ClientGuiController();
+        clientGuiController.run();
+    }
+
+}
