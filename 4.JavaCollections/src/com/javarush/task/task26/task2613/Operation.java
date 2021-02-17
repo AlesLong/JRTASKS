d52b27f1c355aeb721f6f8a2be8fcf93829aa976
AlesLong <yevbes@gmail.com>
2021-02-17 16:51:46 +0200

    [Task2613(CashMachine)]

--- 4.JavaCollections/src/com/javarush/task/task26/task2613/Operation.java
+++ 4.JavaCollections/src/com/javarush/task/task26/task2613/Operation.java
@@ -1,25 +1,21 @@
 package com.javarush.task.task26.task2613;
 
 public enum Operation {
+    LOGIN,
     INFO,
     DEPOSIT,
     WITHDRAW,
     EXIT;
 
-   public static Operation getAllowableOperationByOrdinal(Integer i) {
-        if (i == 1) {
-            return INFO;
-        }
-        if (i == 2) {
-            return DEPOSIT;
-        }
-        if (i == 3) {
-            return WITHDRAW;
-        }
-        if (i == 4) {
-            return EXIT;
-        } else {
+    public static Operation getAllowableOperationByOrdinal(Integer i) {
+        if (i == LOGIN.ordinal()) {
             throw new IllegalArgumentException();
         }
+        for (Operation o : values()) {
+            if (o.ordinal() == i) {
+                return o;
+            }
+        }
+        throw new IllegalArgumentException();
     }
 }
