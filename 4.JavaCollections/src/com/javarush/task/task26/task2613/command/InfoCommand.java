9690aa0305cc037083530d0fd04a509d6cfeeb46
AlesLong <yevbes@gmail.com>
2021-02-17 17:10:12 +0200

    [Task2613(CashMachine)]

--- 4.JavaCollections/src/com/javarush/task/task26/task2613/command/InfoCommand.java
+++ 4.JavaCollections/src/com/javarush/task/task26/task2613/command/InfoCommand.java
@@ -5,6 +5,7 @@ import com.javarush.task.task26.task2613.CurrencyManipulator;
 import com.javarush.task.task26.task2613.CurrencyManipulatorFactory;
 
 class InfoCommand implements Command {
+
     @Override
     public void execute() {
         ConsoleHelper.writeMessage("Information:");
