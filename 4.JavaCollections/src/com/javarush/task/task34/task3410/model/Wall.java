7b4da526a6e2afd551dfe3e57af6dc6daf9dc0ab
AlesLong <yevbes@gmail.com>
2021-02-19 18:55:03 +0200

    [Task3410(Sokoban)]

--- 4.JavaCollections/src/com/javarush/task/task34/task3410/model/Wall.java
+++ 4.JavaCollections/src/com/javarush/task/task34/task3410/model/Wall.java
@@ -2,22 +2,21 @@ package com.javarush.task.task34.task3410.model;
 
 import java.awt.*;
 
-public class Home extends GameObject {
-    public Home(int x, int y) {
+public class Wall extends CollisionObject{
+    public Wall(int x, int y) {
         super(x, y);
-        this.height = 2;
-        this.width = 2;
     }
 
     @Override
     public void draw(Graphics graphics) {
-        graphics.setColor(Color.red);
+        graphics.setColor(Color.DARK_GRAY);
 
         int xc = getX();
         int yc = getY();
         int height = getHeight();
         int width = getWidth();
-        
-        graphics.fillOval(xc - width / 2, yc - height / 2, width, height);
+
+        graphics.drawRect(xc, yc, width, height);
+
     }
 }
