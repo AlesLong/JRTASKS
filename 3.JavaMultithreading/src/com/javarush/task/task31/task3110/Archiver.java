cbe5cce79e4eaf68c05d449b694c4b3e4d9d67ad
AlesLong <yevbes@gmail.com>
2021-02-22 17:26:42 +0200

    [Task3110(Archiver)]

---
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task31.task3110;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.Paths;
+
+public class Archiver {
+    public static void main(String[] args) throws IOException {
+        try(
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in))){
+            while (br.ready()){
+                String path = br.readLine();
+                String pathArchive = br.readLine();
+                ZipFileManager zipFileManager = new ZipFileManager(Paths.get(path));
+                zipFileManager.createZip(Paths.get(pathArchive));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
