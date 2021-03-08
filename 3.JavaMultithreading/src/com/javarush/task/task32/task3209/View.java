dbd8bfe4640021aaf5ced71b03e9107e0f51cd2a
Yevheniy <yevbes@gmail.com>
2021-03-08 16:55:15 +0200

    [Task3209(HTML Editor)]

--- 3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
@@ -120,4 +120,8 @@ public class View extends JFrame implements ActionListener {
     public UndoListener getUndoListener() {
         return undoListener;
     }
+
+    public boolean isHtmlTabSelected() {
+        return tabbedPane.getSelectedIndex() == 0;
+    }
 }
