8ef51fbaa93a282f09f4c701ed7c6aee84131903
Yevheniy <yevbes@gmail.com>
2021-03-09 14:58:39 +0200

    [Task3209(HTML Editor)]

--- 3.JavaMultithreading/src/com/javarush/task/task32/task3209/Controller.java
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/Controller.java
@@ -1,6 +1,10 @@
 package com.javarush.task.task32.task3209;
 
+import com.javarush.task.task32.task3209.listeners.UndoListener;
+
+import javax.swing.text.Document;
 import javax.swing.text.html.HTMLDocument;
+import javax.swing.text.html.HTMLEditorKit;
 import java.io.File;
 
 public class Controller {
@@ -32,5 +36,15 @@ public class Controller {
         view.setController(controller);
         view.init();
         controller.init();
+    }
+
+    public void resetDocument() {
+        UndoListener undoListener = view.getUndoListener();
+        if (document != null) {
+            document.removeUndoableEditListener(undoListener);
+        }
+        document = (HTMLDocument) new HTMLEditorKit().createDefaultDocument();
+        document.addUndoableEditListener(undoListener);
+        view.update();
     }
 }
