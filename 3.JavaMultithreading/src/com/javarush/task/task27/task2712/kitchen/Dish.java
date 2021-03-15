e0b10875683a38ab170f57d92ddcab5868869a0b
Yevheniy <yevbes@gmail.com>
2021-03-15 13:58:10 +0200

    [Task2712(RestaurantApp)]

---
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/kitchen/Dish.java
@@ -0,0 +1,13 @@
+package com.javarush.task.task27.task2712.kitchen;
+
+public enum Dish {
+    FISH,
+    STEAK,
+    SOUP,
+    JUICE,
+    WATER;
+
+    public static String allDishesToString(){
+        return "FISH, STEAK, SOUP, JUICE, WATER";
+    }
+}
