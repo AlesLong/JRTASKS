5c35f7124e679a586e37112fcfba82badb160d40
Yevheniy <yevbes@gmail.com>
2021-03-22 14:14:07 +0200

    [Task2712(Restaurant app)]

--- 3.JavaMultithreading/src/com/javarush/task/task27/task2712/Restaurant.java
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/Restaurant.java
@@ -2,13 +2,17 @@ package com.javarush.task.task27.task2712;
 
 import com.javarush.task.task27.task2712.kitchen.Cook;
 import com.javarush.task.task27.task2712.kitchen.Waiter;
+import com.javarush.task.task27.task2712.statistic.StatisticManager;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Restaurant {
 
     private static final int ORDER_CREATING_INTERVAL = 100;
 
     public static void main(String[] args) {
-        Tablet tablet = new Tablet(5);
+        
 
         Waiter waiter = new Waiter();
 
@@ -18,15 +22,34 @@ public class Restaurant {
         directorTablet.printAdvertisementProfit();
         directorTablet.printArchivedVideoSet();
 
-        Cook cook = new Cook("Yevheniy");
-        tablet.addObserver(cook);
-        cook.addObserver(waiter);
+        Cook cookYevheniy = new Cook("Yevheniy");
+        Cook cookRoman = new Cook("Roman");
+        StatisticManager instance = StatisticManager.getInstance();
+        instance.register(cookRoman);
+        instance.register(cookYevheniy);
+
+        List<Tablet> tablets = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            tablets.add(new Tablet(i + 1));
+        }
 
 
-        tablet.createOrder();
-        tablet.createOrder();
-        tablet.createOrder();
-        tablet.createOrder();
+        for (Tablet tablet :
+                tablets) {
+            tablet.addObserver(cookRoman);
+            tablet.addObserver(cookYevheniy);
+        }
+
+        Thread thread = new Thread(new RandomOrderGeneratorTask(tablets, ORDER_CREATING_INTERVAL));
+        thread.start();
+
+        try {
+            Thread.sleep(1000);
+            thread.interrupt();
+            thread.join();
+            Thread.sleep(1000);
+        } catch (InterruptedException ignore) {
+        }
 
     }
 }
